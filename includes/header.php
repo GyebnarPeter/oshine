e8e5907f5423c98a471c9fff766eedb3f3b87c6a
FerkovicsLionel <48321205+FerkovicsLionel@users.noreply.github.com>
2021-01-13 14:40:39 +0100

    i created the base files

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="shortcut icon" type="image/png" href="./img/favicon.png"/>
+    <link rel="stylesheet" href="./libs/css/main.css">
+    <title>Oshine</title>
+</head>
+<body>
+

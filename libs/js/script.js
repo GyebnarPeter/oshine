10b95007c2e406176c59eb9efc0de35afd26e79a
GyebnarPeter <gyebnarpetter@gmail.com>
2021-01-21 15:53:44 +0100

    gallery fade effect

--- libs/js/script.js
+++ libs/js/script.js
@@ -2,3 +2,12 @@
 $(".menu-btn").click( () => {
     $(".desktop-header").slideToggle();
 });
+
+
+// Gallery fade effect
+var speed = 400;
+$('.main-gallery > a').each( function(item) {
+  $(this).delay(item*speed*0.25).animate(
+    { 'opacity':'0.5' },speed ).animate(
+    { 'opacity':'1' },0.5*speed );
+});
